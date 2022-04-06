6ccf49e6013ba080502ca6876979222a1e24902d
shubhamdixit863 <shubhamdixit863@gmail.com>
2022-04-06 15:55:39 +0530

    react app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+    <div className="App">
+     
+
+      <Home/>
+     
+ 
+    </div>
+  )
+}
+
+export default App
